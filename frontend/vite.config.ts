6f74f4ff7a4f18dc33526e0e0878f71c4b966f46
Yeoh-Yi-Hang <yh_yeoh10@hotmail.com>
2024-10-11 11:52:16 +0800

    chore: enable relative base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from 'fs';
+import { defineConfig } from "vite";
 
 // Define a type for environment variables
-interface Environment {
-    VITE_BASE?: string;
-}
-
-// Function to read environment variables from .env file
-function getEnv(): Environment {
-    const env: Environment = {};
-    const envFile = fs.readFileSync('.env', 'utf8');
-    envFile.split('\n').forEach(line => {
-        const [key, value] = line.split('=');
-        env[key.trim()] = value.trim();
-    });
-    return env;
-}
-
-const env = getEnv();
 
 export default defineConfig({
-    base: env.VITE_BASE || '/', // Using the value from the environment variable or default to '/'
+    base: './', // Using the value from the environment variable or default to '/'
     plugins: [
         react(),
     ],
@@ -35,6 +18,6 @@ export default defineConfig({
             "/ask": "http://localhost:5000",
             "/chat": "http://localhost:5000"
         },
-        open: env.VITE_BASE || '/', // Using the value from the environment variable or default to '/'
+        open: './', // Using the value from the environment variable or default to '/'
     }
 });
