65c85cdce571667fe495ad39ea9549f9e5ac7307
Yeoh-Yi-Hang <yh_yeoh10@hotmail.com>
2024-10-17 16:35:44 +0800

    cicd: reads APP_BASE_PATH from env and delete gh action

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,28 @@
 import react from "@vitejs/plugin-react";
+import fs from 'fs';
+import path from "path";
 import { defineConfig } from "vite";
+
+interface Environment {
+    APP_BASE_PATH?: string;
+}
+// Function to read environment variables from .env file
+function getEnv(): Environment {
+    const env: Environment = {};
+    const envFile = fs.readFileSync(path.resolve(__dirname, "../.env"), 'utf8');
+    envFile.split('\n').forEach(line => {
+        const kvs = line.split('=');
+        if (kvs.length == 2) {
+            env[kvs[0].trim()] = kvs[1].trim();
+        }
+    });
+    return env;
+}
+const env = getEnv();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://maygpt.maybankazuresandbox.com/compliance",
+    base: env.APP_BASE_PATH,
     plugins: [react()],
     build: {
         outDir: "../static",
