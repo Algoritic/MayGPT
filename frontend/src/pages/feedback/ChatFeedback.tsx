f7158001a50698f47414c5ab02976b1a217bf2e8
Yeoh-Yi-Hang <yh_yeoh10@hotmail.com>
2024-10-25 15:07:03 +0800

    fe: use env var for feedback url

--- frontend/src/pages/feedback/ChatFeedback.tsx
+++ frontend/src/pages/feedback/ChatFeedback.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { Dialog, CommandBarButton } from '@fluentui/react';
-import styles from './ChatFeedbackButton.module.css';
+import { CommandBarButton, Dialog } from "@fluentui/react";
+import { useState } from "react";
+import styles from "./ChatFeedbackButton.module.css";
 
 const ChatFeedbackButton = () => {
   const [isDialogVisible, setIsDialogVisible] = useState(false);
-  const surveyUrl = 'https://mpulse.maybanksandbox.com/index.php/638311?lang=en';
-  
+  const surveyUrl = import.meta.env.VITE_SURVEY_PATH;
+
   // This function toggles the visibility of the dialog
   const showDialog = () => {
     setIsDialogVisible(true);
@@ -19,7 +19,7 @@ const ChatFeedbackButton = () => {
   return (
     <>
       <CommandBarButton
-        iconProps={{ iconName: 'Feedback' }} // Make sure to use an existing icon name
+        iconProps={{ iconName: "Feedback" }} // Make sure to use an existing icon name
         title="Feedback"
         ariaLabel="Feedback"
         text="Feedback"
@@ -30,27 +30,34 @@ const ChatFeedbackButton = () => {
         hidden={!isDialogVisible}
         onDismiss={closeDialog}
         dialogContentProps={{
-          title: 'Feedback',
-          showCloseButton: true
+          title: "Feedback",
+          showCloseButton: true,
         }}
         styles={{
-            main: [
-                {
-                selectors: {
-                    ['@media (min-width: 480px)']: {
-                        minWidth: '1200px',
-                        background: "#FFFFFF",
-                        boxShadow: "0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)",
-                        borderRadius: "8px",
-                        maxHeight: '600px',
-                        minHeight: '100px',
-                    }
+          main: [
+            {
+              selectors: {
+                ["@media (min-width: 480px)"]: {
+                  minWidth: "1200px",
+                  background: "#FFFFFF",
+                  boxShadow:
+                    "0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)",
+                  borderRadius: "8px",
+                  maxHeight: "600px",
+                  minHeight: "100px",
                 },
+              },
             },
-        ]
+          ],
         }}
       >
-       <iframe src={surveyUrl} width="100%" height="1000" frameBorder="0" title="MPulse Survey" />
+        <iframe
+          src={surveyUrl}
+          width="100%"
+          height="1000"
+          frameBorder="0"
+          title="MPulse Survey"
+        />
       </Dialog>
     </>
   );
